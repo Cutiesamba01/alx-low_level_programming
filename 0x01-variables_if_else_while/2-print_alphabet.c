1b54bbe7123a30fc42f01dd2b4bec80a42830554
Cutiesamba01 <rhodagbemi@gmail.com>
2024-06-25 04:29:57 +0100

    the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,7 +10,8 @@ int main(void)
 {
 	char r;
 
-		for (r = 'a'; r <= 'z' || r <= 'Z'; r++);
+	for (r = 'a'; r <= 'z' || r <= 'Z'; r++)
+
 	{
 		putchar(r);
 		if (r == 'Z')
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * uppercase and lowercase code
+ * Return: Always return 0
+ */
+
+int man(void)
+
+{
+	char r;
+
+	for (
+
+}
