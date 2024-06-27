786b68346ee24b1e5a138f1934e5528491187097
Cutiesamba01 <rhodagbemi@gmail.com>
2024-06-27 05:25:41 +0100

    natural numbers with conditions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * main - This function finds and prints the Fibonacci number 244293.
+ * Return: Always return 0.
+ */
+
+int main(void)
+
+{
+	long j = 1, k = 2, temp;
+	bool found = false;
+
+	while (k != 244293)
+	{
+		temp = k;
+		k = k + j;
+		j = temp;
+
+		if (k > 244293)
+		{
+			found = false;
+			break;
+		}
+	}
+
+	if (found)
+	{
+		printf("%ld\n (7 chars long)\n", k);
+	}
+	else
+	{
+		fprintf(stderr, "[Anything]\n(0 chars long)\n");
+	}
+
+	return (0);
+}
