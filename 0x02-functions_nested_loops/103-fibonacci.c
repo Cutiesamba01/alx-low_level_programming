16d5bf73e3729360c312c91816ee0168ad55023a
Cutiesamba01 <rhodagbemi@gmail.com>
2024-06-27 05:44:24 +0100

    second fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main printing funtion
+ * less than 4000000.
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	long i = 1, j = 1, k = 2, sum = 2;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+		{
+			sum += k;
+		}
+		i = j;
+		j = k;
+	}
+	printf("%ld ", sum);
+	return (0);
+}
