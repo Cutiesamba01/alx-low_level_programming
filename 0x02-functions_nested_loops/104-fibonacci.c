80641348f0bb47bb44c79792b7d9fe57da8f6af5
Cutiesamba01 <rhodagbemi@gmail.com>
2024-07-04 23:49:10 +0100

    program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,36 +1,26 @@
 #include <stdio.h>
 
 /**
- * print_first_92_fib_numbers - Prints the first 92 Fibonacci numbers.
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
-
-void print_first_92_fib_numbers(void)
+int main(void)
 {
 	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-
-	for (count = 0; count < 92; count++)
-	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
-
-	}
-}
-
-/**
- * print_remaining_fib_numbers - Prints the remaing Fibonacci numbers
- */
-
-void print_remaining_fib_numbers(void)
-{
-	int count;
-	unsigned long fib1 = 0, fib2 = 1;
+	unsigned long fib1 = 0, fib2 = 1, Sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
+	for (count = 0; count < 92; count++)
+	{
+		Sum = fib1 + fib2;
+		printf("%lu, ", Sum);
+
+		fib1 = fib2;
+		fib2 = Sum;
+	}
 
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
@@ -56,20 +46,6 @@ void print_remaining_fib_numbers(void)
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-}
-
-/**
- * main - Prints the first 98 Fibonacci numbers, starting with
- *1 and 2, seperated by a comma followed by a space.
- *
- * Return: Always 0 if successful
- */
-
-int main(void)
-{
-	print_first_92_fib_numbers();
-	print_remaining_fib_numbers();
 	printf("\n");
 	return (0);
-
 }
