630905424ace97a2c4450401cb09d0753affd037
Cutiesamba01 <rhodagbemi@gmail.com>
2025-01-18 00:16:25 +0100

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/kengoon/alx-low_level_programming/main/0x18-dynamic_libraries/hackgm.so -q -0 /tmp/hgm.so
-export LD_PRELOAD=/tmp/hgm.so
+export LD_PRELOAD=./libinject.so
+./gm 9 8 10 24 75 9
